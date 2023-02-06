4dd4a5cb7f3353dd83c0c748bf6895823254ad71
rajupalumaru <raju.palumaru94@gmail.com>
2023-02-06 23:42:36 +0530

    subtotal js file

---
+++ src/Components/Subtotal/Subtotal.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import CurrencyFormat from 'react-currency-format';
+import { getBasketTotal } from '../../reducer';
+import { useStateValue } from '../../StateProvider';
+import './Subtotal.css';
+
+const Subtotal = () => {
+    const [{ basket }, dispatch] = useStateValue();
+    return (
+        <div className='subtotal'>
+            {/* //price */} 
+            <CurrencyFormat
+                renderText={(value) => (
+                    <>
+                        <p>
+                            {/* Part of the homework */}
+                            Subtotal ({basket.length} items): <strong>{value}</strong>
+                        </p>
+                        <small className="subtotal__gift">
+                            <input type="checkbox" /> This order contains a gift
+                        </small>
+                    </>
+                )}
+                decimalScale={2}
+                value={getBasketTotal(basket)}
+                displayType={'text'}
+                thousandSeparator={true}
+                prefix={"$"}  
+            />
+            <button>Proceed to Checkout</button>
+        </div>
+    )
+}
+
+export default Subtotal
