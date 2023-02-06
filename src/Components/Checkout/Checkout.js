9af9296f9cfbe348a9ef2daab870014abed7b45e
rajupalumaru <raju.palumaru94@gmail.com>
2023-02-06 23:40:57 +0530

    subtotal Page

--- src/Components/Checkout/Checkout.js
+++ src/Components/Checkout/Checkout.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useStateValue } from '../../StateProvider';
 import CheckoutProduct from '../CheckoutProduct/CheckoutProduct';
+import Subtotal from '../Subtotal/Subtotal';
 import './Checkout.css';
 
 const Checkout = () => {
@@ -8,34 +9,43 @@ const Checkout = () => {
     console.log("checkout", basket)
     return (
         <div className='checkout'>
-            <img
-                className="checkout__ad"
-                src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
-                alt=""
-            />
-            {
-                basket?.length === 0 ? (
-                    <div>
-                        <h2>Your Shopping Basket is Empty</h2>
-                        <p>You have no Items in your basket.To buy or more items,
-                            click "Add To Basket" next to the item</p>
-                    </div>
-                ) : (
-                    <div>
-                        <h1 className='checkout_title'>Your Shopping Basket</h1>
-                        {/* List out All of the checkout product */}
-                        {
-                            basket.map(item => (
-                                <CheckoutProduct 
-                                    id={item.id}
-                                    title={item.title}
-                                    price={item.price}
-                                    image={item.image}
-                                    rating={item.rating}
+            <div className='checkout_left'>
+                <img
+                    className="checkout__ad"
+                    src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
+                    alt=""
+                />
+                {
+                    basket?.length === 0 ? (
+                        <div>
+                            <h2>Your Shopping Basket is Empty</h2>
+                            <p>You have no Items in your basket.To buy or more items,
+                                click "Add To Basket" next to the item</p>
+                        </div>
+                    ) : (
+                        <div>
+                            <h1 className='checkout_title'>Your Shopping Basket</h1>
+                            {/* List out All of the checkout product */}
+                            {
+                                basket.map(item => (
+                                    <CheckoutProduct
+                                        id={item.id}
+                                        title={item.title}
+                                        price={item.price}
+                                        image={item.image}
+                                        rating={item.rating}
 
-                                />
-                            ))
-                        }
+                                    />
+                                ))
+                            }
+                        </div>
+                    )
+                }
+            </div>
+            {
+                basket.length > 0 && (
+                    <div className='checkout_right'>
+                        <Subtotal />
                     </div>
                 )
             }
